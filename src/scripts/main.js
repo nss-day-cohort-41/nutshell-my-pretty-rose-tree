9f2a88aa869faef68fc0db520c6bd4107fc24ffb
Faith Magras <bdanielle092@gmail.com>
2020-06-29 22:51:05 -0500

    working on task.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
-// import taskName from "./Task.js"
+import taskName from "./Task/Tasks.js"
 
 // Import modules
 import loginRegisterListeners from '././login_registration/loginListeners.js'
 import API from '././login_registration/loginAPICall.js'
-// taskName();
+taskName.taskForm();
 //// Registration and Login Components ////
 
 // Invoking method to get all users from the API 
