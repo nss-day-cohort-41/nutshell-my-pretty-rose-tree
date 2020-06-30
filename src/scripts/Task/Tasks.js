09957543c088bb7529253488c0051d13f5f7e676
Faith Magras <bdanielle092@gmail.com>
2020-06-30 09:15:14 -0500

    added comments

--- src/scripts/Task/Tasks.js
+++ src/scripts/Task/Tasks.js
@@ -3,19 +3,16 @@ taskForm: () => {
     document.querySelector(".taskForm").innerHTML = `
     <form>
     <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      <label for="exampleInputEmail1">Task</label>
+      <input type="task" class="form-control" id="task" aria-describedby="" placeholder="enter task">
+      <small id="task"
     </div>
     <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      <label for="taskDate">Date to be complete</label>
+      <input type="taskDate" class="form-control" id="taskDate" placeholder="enter date">
     </div>
-    <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+
+    <button type="taskSubmit" class="btn btn-dark">Submit</button>
   </form>`
 }
 }
