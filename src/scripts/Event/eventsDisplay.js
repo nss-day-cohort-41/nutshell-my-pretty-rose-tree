bbd8772d9b7dfb9f2e0083aedb3c19e2374abddf
James Su-Shum <sushumjames@gmail.com>
2020-07-01 15:55:05 -0500

    made notes to display events function

--- src/scripts/Event/eventsDisplay.js
+++ src/scripts/Event/eventsDisplay.js
@@ -1,11 +1,13 @@
 import eventsHTML from "./eventsHtmlComponent.js"
 
+// Method displays events to event list container in DOM
 const eventsDisplay = {
   displayEventsList(arrayOfEvents) {
     const eventListElement = document.getElementById('eventList')
     eventListElement.innerHTML = ""
     eventListElement.innerHTML = `<hr><h4 id="eventsListTitle">Upcoming Events</h4><hr>`
 
+    // Sorts the dates in all event objects placing the latest event first
     const sortedArray = arrayOfEvents.sort((a, b) => {
       return new Date(b.date) - new Date(a.date)
     })
